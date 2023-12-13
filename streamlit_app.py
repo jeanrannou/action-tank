26582fb2d50561b080ffa10827bb9e3035c80266
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2023-12-13 11:51:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('Action tank project')
+
+@st.cahe_data
+def load_data(path: str):
+  data = pd.read_excel(path)
+  return data
+
+df = load_data("./Logements sociaux Paris.xlsx")
+st.dataframe(df)
