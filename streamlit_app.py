0d6376de4224a430621d72db01f915c6f652e353
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2023-12-13 11:58:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ def load_data(path: str):
   data = pd.read_excel(path)
   return data
 
-df = load_data("main/Logements sociaux Paris.xlsx")
+df = load_data("https://github.com/jeanrannou/action-tank/blob/794a072e1079e6fb09cd5e4fde7949f79a59cc38/Logements%20sociaux%20Paris.xlsx?raw=true")
 st.dataframe(df)
