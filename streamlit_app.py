6c803af73d9c7a62142bd1483505cd00b4800e92
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2023-12-13 12:08:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Action tank')
+st.title('Actionnn')
 
 # URL du fichier Excel sur GitHub
 url = 'https://github.com/jeanrannou/action-tank/blob/main/Logements%20sociaux%20Paris.xlsx?raw=true'
