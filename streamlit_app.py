60bb5faa810e0372aa739aa2406113680c9951f1
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2023-12-13 11:59:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
-st.title('Action tank project')
+st.title('Action tank')
 
-@st.cache_data
-def load_data(path: str):
-  data = pd.read_excel(path)
-  return data
+# URL du fichier Excel sur GitHub
+url = 'https://github.com/jeanrannou/action-tank/blob/main/Logements%20sociaux%20Paris.xlsx'
 
-df = load_data("https://github.com/jeanrannou/action-tank/blob/794a072e1079e6fb09cd5e4fde7949f79a59cc38/Logements%20sociaux%20Paris.xlsx?raw=true")
-st.dataframe(df)
+# Lire le fichier Excel
+df = pd.read_excel(url)
+
+# Afficher le DataFrame
+st.write(df)
