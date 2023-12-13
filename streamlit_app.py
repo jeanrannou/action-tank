a676631dcc8ff6e6d6679fa41aa3fc8cc9992a25
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2023-12-13 18:24:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,8 @@ st.title("Yooo")
 df = pd.read_excel("LogementsSociauxParis.xlsx")
 st.dataframe(df)
 df["Adresse du programme"][1]*3
+
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
