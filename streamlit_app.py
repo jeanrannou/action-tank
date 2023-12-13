9527811e03155e4e351a940fe1b6deb4538c1c4c
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2023-12-13 19:00:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,15 +15,15 @@ st.subheader("1.A. Combien de logements sociaux ont été financés en tout ?")
 st.write('Pour répondre à cette question, nous ')
 
 st.subheader('1.B. Combien de logements du bailleur "IMM 3F" ont été financés en 2015 ?')
-filtered_df1 = df[df['Année du financement - agrément'] == 2015]
+filtered_df1 = df[df['Année du financement - agrément'] == '2015']
 filtered_df2 = filtered_df1[filtered_df1['Bailleur social'] == 'IMM 3F']
 # Calculez le nombre d'occurrences de chaque élément dans la colonne d'intérêt
-occurrences = filtered_df2['Nombre total de logements financés'].value_counts()
+sumvalues = filtered_df2['Nombre total de logements financés'].sum()
 
 # Affichez les résultats dans Streamlit
-st.write(occurrences)
+st.write(sumvalues)
 
-st.bar_chart(occurrences)
+st.bar_chart(sumvalues)
 
 st.title('Titre 2')
 st.write('Contenu pour le Titre 2')
