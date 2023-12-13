bff259267ea6c15a1eaca86229bc98b957b8a59f
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2023-12-13 18:55:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,24 @@ df = pd.read_excel("LogementsSociauxParis.xlsx")
 st.dataframe(df)
 df["Adresse du programme"][1]*3
 
-st.title('Titre 1')
-st.write('Contenu pour le Titre 1')
+st.title('Partie 1 : Explorer les logements sociaux financés à Paris')
+st.subheader("1.A. Combien de logements sociaux ont été financés en tout ?")
+st.write('Pour répondre à cette question, nous ')
+
+st.subheader('1.B. Combien de logements du bailleur "IMM 3F" ont été financés en 2015 ?')
+filtered_df1 = df[df['Année du financement - agrément'] == '2015']
+filtered_df2 = filtered_df1[filtered_df1['Bailleur social'] == 'IMM 3F']
+# Calculez le nombre d'occurrences de chaque élément dans la colonne d'intérêt
+occurrences = filtered_df2['Nombre total de logements financés'].sum()
+
+# Affichez les résultats dans Streamlit
+st.write(occurrences)
+
+st.bar_chart(occurrences)
 
 st.title('Titre 2')
 st.write('Contenu pour le Titre 2')
 
 st.title('Titre 3')
 st.write('Contenu pour le Titre 3')
+
