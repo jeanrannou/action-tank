fefb630da0d54f62ff37a95b434f2d5a14b89ae9
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2023-12-13 16:59:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,28 +4,5 @@ import streamlit as st
 
 
 
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.title("Coucou ohlala")
 
-st.title("Sales Streamlit Dashboard")
-st.markdown("_Prototype v0.4.1_")
-
-with st.sidebar:
-    st.header("Configuration")
-    uploaded_file = st.file_uploader("Choose a file")
-
-if uploaded_file is None:
-    st.info(" Upload a file through config", icon="ℹ️")
-    st.stop()
-
-#######################################
-# DATA LOADING
-#######################################
-
-
-@st.cache_data
-def load_data(path: str):
-    df = pd.read_excel(path)
-    return df
-
-
-df = load_data(uploaded_file)
