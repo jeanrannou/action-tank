8b17b3c2202f24a80fa2cac54989cfd11c182bcd
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2023-12-13 12:31:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
+import random
+import duckdb
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+
+
+
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 st.title("Sales Streamlit Dashboard")
